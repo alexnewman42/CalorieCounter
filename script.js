a38a6a938a50cca39cc10988663e47ac9c7ea6d8
root <alexnewman42@gmail.com>
2024-03-20 08:27:11 -0400

    Create calculateCalories function

--- script.js
+++ script.js
@@ -45,4 +45,9 @@ function getCaloriesFromInputs(list) {
     return calories;
 }
 
+function calculateCalories(e) {
+    e.preventDefault();
+    isError = false;
+}
+
 addEntryButton.addEventListener('click', addEntry);
