7ba7c8509774485e546bf8766077738dce97453a
root <alexnewman42@gmail.com>
2024-03-21 08:27:10 -0400

    Finish calculateCalories function

--- script.js
+++ script.js
@@ -70,7 +70,13 @@ function calculateCalories(e) {
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
     const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
 
-    output.innHTML = `<span class"${surplusOrDeficit.toLowerCase()}">${remainingCalories} Calorie ${surplusOrDeficit}</span>`;
+    output.innHTML = `<span class"${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+    <hr>
+    <p>${budgetCalories} Calories Budgeted</p>
+    <p>${consumedCalories} Calories Consumed</p>
+    <p>${exerciseCalories} Calories Burned</p>
+    `;
+    output.classList.remove('hide');
 }
 
 addEntryButton.addEventListener('click', addEntry);
